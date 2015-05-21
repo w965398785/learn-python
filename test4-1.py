de6769a7a6e118c3a9737e77fa56dda5d61b045b
wangbensong <wbs7721@qq.com>
2015-05-21 20:40:24 +0800

    Signed-off-by: wangbensong <wbs7721@qq.com>

--- test4-1.py
+++ test4-1.py
@@ -40,7 +40,53 @@ for row in cursor:
    print "ID = ", row[0]
    print "NAME = ", row[1]
    print "sex = ", row[2]
-   print "score = ", row[3], "\n"
+   print "score = ", row[3],
+   print "AGE = " row[4] "\n"
 
 print "Operation done successfully";
 conn.close()
+import sqlite3
+
+conn = sqlite3.connect('student.db')
+print "Opened database successfully";
+
+cursor = conn.execute("SELECT id, name, sex, score from people")
+for row in cursor:
+   print "ID = ", row[0]
+   print "NAME = ", row[1]
+   print "sex = ", row[2]
+   print "score = ", row[3],
+   print "AGE = " row[4]   "\n"
+
+print "Operation done successfully";
+conn.close()
+import sqlite3
+
+conn = sqlite3.connect('student.db')
+print "Opened database successfully";
+
+conn.execute("UPDATE people set score = 26 where ID=1")
+conn.commit
+print "Total number of rows updated :", conn.total_changes
+
+cursor = conn.execute("SELECT id, name, sex, score from people")
+for row in cursor:
+   print "ID = ", row[0]
+   print "NAME = ", row[1]
+   print "sex = ", row[2]
+   print "score = ", row[3],
+   print "AGE = ", row[4]   "\n"
+
+print "Operation done successfully";
+conn.close()
+
+
+conn = sqlite3.connect('student.db')
+print "Opended database successfully";
+cursor = conn.execute("SELECT ID,name,AGE,sex,score from people")
+for row in cursor:
+    print "NAME = ", row[1],"AGE = ",rows[2],"sex = ",row[3], "score = ",row[4]
+	
+print "Operation done successfully";
+conn.close
+
